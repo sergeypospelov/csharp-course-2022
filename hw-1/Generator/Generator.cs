cc46b47257fd9de0942d73c0a483b7a95f08f070
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-09-14 17:21:10 +0300

    add hw-1 task-2

---
+++ hw-1/Generator/Generator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generator
+{
+    public static class Generator
+    {
+        private static readonly Random Random = new Random();
+
+        public static string GeneratePassword()
+        {
+            var len = Random.Next(6, 21);
+
+            var password = new char[len];
+
+            var permutation = RandomSet(len, len).ToArray();
+            
+            // underscore
+            password[permutation[0]] = '_';
+            password[permutation[1]] = RandomLetter(true);
+            password[permutation[2]] = RandomLetter(true);
+
+            for (var i = 3; i < Math.Min(len, 8); i++)
+            {
+                var idx = permutation[i];
+                if (!(idx > 0 && char.IsDigit(password[idx - 1]) || idx + 1 < len && char.IsDigit(password[idx + 1])))
+                {
+                    password[idx] = RandomDigit();
+                }
+            }
+
+            for (var i = 0; i < len; i++)
+            {
+                if (password[i] == default)
+                {
+                    password[i] = RandomLetter();
+                }
+            }
+
+            return new string(password);
+        }
+
+        private static char RandomDigit()
+        {
+            return (char)('0' + Random.Next(10));
+        }
+        private static char RandomLetter(bool upper = false)
+        {
+            int first = upper ? 'A' : 'a';
+            return (char)(first + Random.Next(26));
+        }
+
+        private static IEnumerable<int> RandomSet(int n, int k)
+        {
+            var randomSet = Enumerable.Range(0, k)
+                .Select(_ => Random.Next(n - k))
+                .OrderBy(i => i)
+                .Select((i, idx) => i + idx)
+                .OrderBy(_ => Random.Next()); // random shuffle
+
+            return randomSet;
+        }
+    }
+}
---
+++ hw-1/Generator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Generator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var password = Generator.GeneratePassword();
+            Console.Write(password);
+        }
+    }
+}
