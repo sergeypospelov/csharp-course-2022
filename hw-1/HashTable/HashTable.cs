ce7662e8f33b02691259828ca5ecf3a2a297bc03
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-09-14 15:47:08 +0300

    Add hw-0 task-1

---
+++ hw-1/HashTable/HashTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HashTable
+{
+    public class HashTable<TK, TV>
+    {
+        private const int DefaultCapacity = 100;
+        
+        private readonly List<KeyValuePair<TK, TV>> []_table;
+
+        public HashTable(int capacity = DefaultCapacity)
+        {
+            if (capacity < 1)
+            {
+                throw new ArgumentException($"Capacity must be greater than zero, got {capacity}");
+            }
+            _table = Enumerable.Range(0, capacity).Select(_ => new List<KeyValuePair<TK, TV>>()).ToArray();
+        }
+
+        public void Remove(TK key)
+        {
+            var index = ComputeBaseIndex(key);
+            _table[index].RemoveAll(pr => pr.Key.Equals(key));
+            
+        }
+
+        public void Add(TK key, TV value)
+        {
+            Remove(key);
+            var index = ComputeBaseIndex(key);
+            _table[index].Add(new KeyValuePair<TK, TV>(key, value));
+        }
+
+        public bool Exists(TK key)
+        {
+            var index = ComputeBaseIndex(key);
+            return _table[index].Exists(pr => pr.Key.Equals(key));
+        }
+
+        public TV Get(TK key)
+        {
+            if (!Exists(key))
+            {
+                return default;
+            }
+            var index = ComputeBaseIndex(key);
+            var value = _table[index].Find(pr => pr.Key.Equals(key));
+            return value.Value;
+        }
+
+        private int ComputeBaseIndex(TK key)
+        {
+            return (key.GetHashCode() % _table.Length + _table.Length) % _table.Length;
+        }
+    }
+}
---
+++ hw-1/HashTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace HashTable
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var weekDays = new HashTable<string, string>(3);
+
+            weekDays.Add("Monday", "Понедельник");
+            weekDays.Add("Tuesday", "Вторник");
+            weekDays.Add("Wednesday", "Среда");
+            weekDays.Add("Thursday", "Четверг");
+            weekDays.Add("Friday", "Пятница");
+            weekDays.Add("Saturday", "Суббота");
+            weekDays.Add("Sunday", "Воскресенье");
+
+            while (true)
+            {
+                var day = Console.ReadLine();
+                var russianDay = weekDays.Get(day);
+
+                Console.Out.WriteLine(russianDay != null
+                    ? $"{day} in Russian is {russianDay}"
+                    : $"I don't know {day}!"
+                );
+            }
+        }
+    }
+}
