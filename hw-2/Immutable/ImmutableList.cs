bfd9b1bd5efa24e5f22b86e2b9976ecb5f789935
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-09-17 16:47:26 +0300

    Add: hw-2 task-2

---
+++ hw-2/Immutable/ImmutableList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Immutable
+{
+    public class ImmutableList<T> : IEnumerable<T>
+    {
+
+        private readonly List<T> list_;
+
+        public ImmutableList(List<T> list)
+        {
+            list_ = list;
+        }
+
+        public int Count => list_.Count;
+
+        public ImmutableList<T> Add(T val)
+        {
+            var list = list_.ToList();
+            list.Add(val);
+            return new ImmutableList<T>(list);
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return list_[i];
+            }
+        }
+
+        public ImmutableList<T> Remove(T val)
+        {
+            var list = list_.ToList();
+            list.Remove(val);
+            return new ImmutableList<T>(list);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ hw-2/Immutable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Immutable
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new ImmutableList<int>(new List<int> { 1, 2, 3 });
+
+            Console.Out.WriteLine("Old list:");
+            foreach (var i in list)
+            {
+                Console.Out.Write($"{i} ");
+            }
+            Console.Out.WriteLine();
+
+            var newList = list.Add(1).Add(2).Remove(3);
+
+            Console.Out.WriteLine("New list:");
+            foreach (var i in newList)
+            {
+                Console.Out.Write($"{i} ");
+            }
+            Console.Out.WriteLine();
+        }
+    }
+}
