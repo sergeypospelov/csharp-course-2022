0e9b9ac78a45bb18fa5c206396debe34c368687e
Sergey Pospelov <sergeypospelov59@gmail.com>
2022-09-17 16:08:19 +0300

    Add: hw-2 task-4

---
+++ hw-2/Rolls/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Rolls
+{
+    internal static class Program
+    {
+        public static ulong DiceRoll(uint cubes, uint value)
+        {
+            if (value > cubes * 6)
+            {
+                return 0;
+            }
+            
+            var cnt = new ulong[cubes * 6 + 1];
+            cnt[0] = 1;
+            
+            for (int c = 1; c <= cubes; c++)
+            {
+                for (int val = c * 6; val >= 0; val--)
+                {
+                    cnt[val] = 0;
+                    for (int prv = val - 1; prv >= Math.Max(0, val - 6); prv--)
+                    {
+                        cnt[val] += cnt[prv];
+                    }
+                }
+            }
+
+            return cnt[value];
+        }
+        
+        public static void Main(string[] args)
+        {
+            var input = Console.In.ReadLine()!.Split(' ').Select(uint.Parse).ToArray();
+            uint cubes = input[0];
+            uint value = input[1];
+            var res = DiceRoll(cubes, value);
+            Console.Out.WriteLine(res);
+        }
+    }
+}
